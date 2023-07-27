7aa00880876a486be574d955cb115f4a2156e513
Yagami-Jiang <119273528+Yagami-Jiang@users.noreply.github.com>
2023-07-27 22:44:00 +0800

    Update psu.py

--- platform/broadcom/sonic-platform-modules-cel/silverstone-v2/pddf/sonic_platform/psu.py
+++ platform/broadcom/sonic-platform-modules-cel/silverstone-v2/pddf/sonic_platform/psu.py
@@ -11,8 +11,6 @@ try:
 except ImportError as e:
     raise ImportError(str(e) + "- required module not found")
 
-PSU_STATUS_INFO_CMD = "i2cget -y -f 100 0x0d 0x60"
-
 
 class Psu(PddfPsu):
     """PDDF Platform-Specific PSU class"""
@@ -28,32 +26,6 @@ class Psu(PddfPsu):
     @staticmethod
     def get_type():
         return 'AC'
-
-    def get_status(self):
-        """
-        Get PSU1/2 power ok status by iic command
-        return: True or False
-        """
-        status, res = self.helper.run_command(PSU_STATUS_INFO_CMD)
-        if not status:
-            return False
-        psu_status = (bin(int(res, 16)))[-2:]
-        psu_power_ok_index = 0 if self.psu_index == 1 else 1
-        psu_power_ok = self.plugin_data['PSU']['psu_power_good']["i2c"]['valmap'][psu_status[psu_power_ok_index]]
-        return psu_power_ok
-
-    def get_presence(self):
-        """
-        Get PSU1/2 present status by iic command
-        return: True or False
-        """
-        status, res = self.helper.run_command(PSU_STATUS_INFO_CMD)
-        if not status:
-            return False
-        psu_status = (bin(int(res, 16)))[-4:-2]
-        psu_present_index = 0 if self.psu_index == 1 else 1
-        psu_present = self.plugin_data['PSU']['psu_present']["i2c"]['valmap'][psu_status[psu_present_index]]
-        return psu_present
 
     @staticmethod
     def get_revision():
